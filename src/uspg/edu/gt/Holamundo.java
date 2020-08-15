525a3acd974c3733d9b1c8961bb5b1a8d956f104
Christian Escobar <caescobarl@alumno.edu.gt>
2020-08-15 15:12:55 -0600

    segundo mensaje

--- src/uspg/edu/gt/Holamundo.java
+++ src/uspg/edu/gt/Holamundo.java
@@ -2,6 +2,7 @@ package uspg.edu.gt;
 public class Holamundo {
 public static void main(String[] args) {
        System.out.println("Hola mundo");
+       System.out.println("segundo hola mundo");
     }
     
 }
